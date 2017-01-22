fe72095765f25c68593d5cf2610436d817313d18
Katie Fritz <katie@katiemfritz.com>
2017-01-22 14:42:11 -0500

    add event listeners for one button to test functionality - just using console.log

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Here\'s some Javascript')
+
+// Variables
+// TODO: Once you have real sounds, update the ids and var names.
+var sound1 = document.getElementById('sound1')
+
+// Methods
+sound1.addEventListener('click', function () {
+  console.log('sound1 click')
+})
+sound1.addEventListener('mouseenter', function () {
+  console.log('sound1 mouseenter')
+})
