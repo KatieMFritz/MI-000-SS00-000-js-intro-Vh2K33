cc923a97b1b14da87122626e5084925326dde2d2
Katie Fritz <katie@katiemfritz.com>
2017-01-22 20:22:14 -0500

    incorporate wav sounds and refactor

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-console.log('Here\'s some Javascript')
+// Snare
+var snareButton = document.getElementById('snareButton')
+var snareSound = new Audio('drum-sounds/snare.wav');
 
-// Variables
-// TODO: Once you have real sounds, update the ids and var names.
-var sound1 = document.getElementById('sound1')
-
-// Methods
-sound1.addEventListener('click', function () {
-  console.log('sound1 click')
+snareButton.addEventListener('click', function () {
+  snareSound.play();
 })
-sound1.addEventListener('mouseenter', function () {
-  console.log('sound1 mouseenter')
+snareButton.addEventListener('mouseenter', function () {
+  snareSound.play();
+})
+
+// Hi Hat
+var hihatButton = document.getElementById('hihatButton')
+var hihatSound = new Audio('drum-sounds/hihat.wav');
+
+hihatButton.addEventListener('click', function () {
+  hihatSound.play();
+})
+hihatButton.addEventListener('mouseenter', function () {
+  hihatSound.play();
+})
+
+// Tink
+var tinkButton = document.getElementById('tinkButton')
+var tinkSound = new Audio('drum-sounds/tink.wav');
+
+tinkButton.addEventListener('click', function () {
+  tinkSound.play();
+})
+tinkButton.addEventListener('mouseenter', function () {
+  tinkSound.play();
 })
