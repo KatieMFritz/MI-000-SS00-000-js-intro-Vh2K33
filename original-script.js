8975faa104a4688c793b00aa3ccc0b79f3c5ab0b
Katie Fritz <katie@katiemfritz.com>
2017-02-04 11:55:41 -0500

    works as intended using audio element

--- original-script.js
+++ original-script.js
@@ -1,32 +1,32 @@
 // Snare
 var snareButton = document.getElementById('snareButton')
-var snareSound = new Audio('drum-sounds/snare.wav');
+var snareSound = new Audio('drum-sounds/snare.wav')
 
 snareButton.addEventListener('click', function () {
-  snareSound.play();
+  snareSound.play()
 })
 snareButton.addEventListener('mouseenter', function () {
-  snareSound.play();
+  snareSound.play()
 })
 
 // Hi Hat
 var hihatButton = document.getElementById('hihatButton')
-var hihatSound = new Audio('drum-sounds/hihat.wav');
+var hihatSound = new Audio('drum-sounds/hihat.wav')
 
 hihatButton.addEventListener('click', function () {
-  hihatSound.play();
+  hihatSound.play()
 })
 hihatButton.addEventListener('mouseenter', function () {
-  hihatSound.play();
+  hihatSound.play()
 })
 
 // Tink
 var tinkButton = document.getElementById('tinkButton')
-var tinkSound = new Audio('drum-sounds/tink.wav');
+var tinkSound = new Audio('drum-sounds/tink.wav')
 
 tinkButton.addEventListener('click', function () {
-  tinkSound.play();
+  tinkSound.play()
 })
 tinkButton.addEventListener('mouseenter', function () {
-  tinkSound.play();
+  tinkSound.play()
 })
--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+/* Snare */
 
+var snareButton = document.getElementById('snare-button')
+var snareSound = document.getElementById('snare-sound')
+
+snareButton.addEventListener('click', function () {
+  snareSound.play()
+})
+
+snareButton.addEventListener('mouseenter', function () {
+  snareSound.play()
+})
+
+/* Tink */
+
+var tinkButton = document.getElementById('tink-button')
+var tinkSound = document.getElementById('tink-sound')
+
+tinkButton.addEventListener('click', function () {
+  tinkSound.play()
+})
+
+tinkButton.addEventListener('mouseenter', function () {
+  tinkSound.play()
+})
+
+/* Tink */
+
+var hihatButton = document.getElementById('hihat-button')
+var hihatSound = document.getElementById('hihat-sound')
+
+hihatButton.addEventListener('click', function () {
+  hihatSound.play()
+})
+
+hihatButton.addEventListener('mouseenter', function () {
+  hihatSound.play()
+})
